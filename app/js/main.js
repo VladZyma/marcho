66d78a6b7b68b03b5b41bf0c0871600c8fa4667f
VladZyma <vladzima13@gmail.com>
2023-03-07 18:43:34 +0200

    added blog standard page

--- app/js/main.js
+++ app/js/main.js
@@ -22,90 +22,97 @@ $(function () {
     fade: true,
   });
 
+  $('.blog-page__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g><path d="M 0.257812 6.382812 C -0.0859375 6.722656 -0.0859375 7.277344 0.257812 7.621094 L 4.632812 11.996094 C 4.972656 12.335938 5.527344 12.335938 5.871094 11.996094 C 6.210938 11.652344 6.210938 11.097656 5.871094 10.757812 L 2.113281 7 L 5.867188 3.242188 C 6.210938 2.902344 6.210938 2.347656 5.867188 2.003906 C 5.527344 1.664062 4.972656 1.664062 4.628906 2.003906 L 0.253906 6.378906 Z M 0.257812 6.382812 "/></g></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g> <path d="M 6.742188 6.382812 C 7.085938 6.722656 7.085938 7.277344 6.742188 7.621094 L 2.367188 11.996094 C 2.027344 12.335938 1.472656 12.335938 1.128906 11.996094 C 0.789062 11.652344 0.789062 11.097656 1.128906 10.757812 L 4.886719 7 L 1.132812 3.242188 C 0.789062 2.902344 0.789062 2.347656 1.132812 2.003906 C 1.472656 1.664062 2.027344 1.664062 2.371094 2.003906 L 6.746094 6.378906 Z M 6.742188 6.382812 " /></g></svg ></button>',
+    infinite: false,
+  });
+
   $('.star').rateYo({
     starWidth: '17px',
     normalFill: '#ccccce',
     ratedFill: '#ffc35b',
     readOnly: true,
+    starSvg: '<svg width="18px" height="16px" viewBox="0 0 18 16" version="1.1"><g><path style=" stroke:none;fill-rule:nonzero;fill-opacity:1;"d="M 9.902344 0.5625 C 9.738281 0.21875 9.386719 0 9.003906 0 C 8.617188 0 8.273438 0.21875 8.101562 0.5625 L 6.09375 4.695312 L 1.605469 5.359375 C 1.230469 5.414062 0.917969 5.679688 0.804688 6.039062 C 0.6875 6.398438 0.78125 6.792969 1.050781 7.058594 L 4.304688 10.28125 L 3.539062 14.835938 C 3.476562 15.210938 3.632812 15.589844 3.941406 15.8125 C 4.25 16.035156 4.660156 16.0625 4.996094 15.882812 L 9.007812 13.742188 L 13.015625 15.882812 C 13.351562 16.0625 13.761719 16.039062 14.070312 15.8125 C 14.382812 15.585938 14.539062 15.210938 14.476562 14.835938 L 13.703125 10.28125 L 16.960938 7.058594 C 17.226562 6.792969 17.324219 6.398438 17.207031 6.039062 C 17.085938 5.679688 16.777344 5.414062 16.402344 5.359375 L 11.914062 4.695312 Z M 9.902344 0.5625 " /></g></svg >',
   });
 
-  $('.filter-price__input').ionRangeSlider({
-    type: 'double',
-    prefix: '$',
-    onStart: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-    },
-    onChange: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-    },
-  });
+$('.filter-price__input').ionRangeSlider({
+  type: 'double',
+  prefix: '$',
+  onStart: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+  onChange: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+});
 
-  $('.select-style, .product-one__item-num').styler();
+$('.select-style, .product-one__item-num').styler();
 
-  $('.shop-content__filter-btn').on('click', function () {
-    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
-    $(this).addClass('shop-content__filter-btn--active');
-  });
-  $('.button-list').on('click', function () {
-    $('.product-item').addClass('product-item--list');
-  });
-  $('.button-grid').on('click', function () {
-    $('.product-item').removeClass('product-item--list');
-  });
+$('.shop-content__filter-btn').on('click', function () {
+  $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+  $(this).addClass('shop-content__filter-btn--active');
+});
+$('.button-list').on('click', function () {
+  $('.product-item').addClass('product-item--list');
+});
+$('.button-grid').on('click', function () {
+  $('.product-item').removeClass('product-item--list');
+});
 
-  $('.product-tabs__top-item').on('click', function (event) {
-    event.preventDefault();
+$('.product-tabs__top-item').on('click', function (event) {
+  event.preventDefault();
 
-    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
-    $(this).addClass('product-tabs__top-item--active');
+  $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+  $(this).addClass('product-tabs__top-item--active');
 
-    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
-    $($(this).attr('href')).addClass('product-tabs__content-item--active');
-  })
+  $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+  $($(this).attr('href')).addClass('product-tabs__content-item--active');
+})
 
 
-  function getTimeRemaining(endtime) {
-    const total = Date.parse(endtime) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-    return {
-      total,
-      days,
-      hours,
-      minutes,
-      seconds
-    };
-  }
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
 
-  function initializeClock(className, endtime) {
-    const clock = document.getElementsByClassName(className)[0];
-    const daysSpan = clock.querySelector('.promo__days');
-    const hoursSpan = clock.querySelector('.promo__hours');
-    const minutesSpan = clock.querySelector('.promo__minutes');
-    const secondsSpan = clock.querySelector('.promo__seconds');
+function initializeClock(className, endtime) {
+  const clock = document.getElementsByClassName(className)[0];
+  const daysSpan = clock.querySelector('.promo__days');
+  const hoursSpan = clock.querySelector('.promo__hours');
+  const minutesSpan = clock.querySelector('.promo__minutes');
+  const secondsSpan = clock.querySelector('.promo__seconds');
 
-    function updateClock() {
-      const t = getTimeRemaining(endtime);
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
 
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
     }
-
-    updateClock();
-    const timeinterval = setInterval(updateClock, 1000);
   }
 
-  const deadline = $('.promo__clock').attr('data-time');
-  initializeClock('promo__clock', deadline);
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+const deadline = $('.promo__clock').attr('data-time');
+initializeClock('promo__clock', deadline);
 })
